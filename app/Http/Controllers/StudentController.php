3380a1c372d80bb014ef8cecd763366870bcbb2d
Rodney <rodney.bakari@strathmore.edu>
2019-06-25 21:47:26 -0700

    change

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -32,7 +32,7 @@ class StudentController extends Controller
         $student->fullName=request('fullName');
         $student->DateOfBirth=request('DateOfBirth');
         $student->address=request('address');
-        $student->balance=request('Balance');
+        $student->balance=request('balance');
         $student->gender=request('gender');
         $student->save();
         return Redirect('/');
